3eda0950b3e3e7fd460e522a4124bcb94fb11418
bogdanR2 <rif99bog@gmail.com>
2022-08-07 11:51:55 +0300

    End of section 7

--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,32 +1,33 @@
 import React from "react";
 import { Button, Card, Image } from "semantic-ui-react";
-import { Activity } from "../../../app/models/activity";
+import LoadingComponent from "../../../app/layout/LoadingComponent";
+import { useStore } from "../../../app/stores/store";
 
-interface Props { 
-    activity: Activity;
-    cancelSelectActivity: () => void;
-    openForm: (id: string) => void;
-}
+export default function ActivtyDetails() {
 
-export default function ActivtyDetails({activity, cancelSelectActivity, openForm} : Props) {
-    return (
-        <Card fluid>
-        <Image src={`/assets/categoryImages/${activity.category}.jpg`}/>
-        <Card.Content>
-          <Card.Header>{activity.title}</Card.Header>
-          <Card.Meta>
-            <span>{activity.date}</span>
-          </Card.Meta>
-          <Card.Description>
-            {activity.description}
-          </Card.Description>
-        </Card.Content>
-        <Card.Content extra>
-          <Button.Group widths='2'>
-            <Button onClick = {() => openForm(activity.id)} basic color='blue' content='Edit'/>
-            <Button onClick={cancelSelectActivity} basic color='grey' content='Cancel'/>
-          </Button.Group>
-        </Card.Content>
-      </Card>
-    )
+  const{activityStore} = useStore();
+  const{selectedActivity: activity, openForm, cancelSelectedActivity} = activityStore;
+
+  if (!activity) return <LoadingComponent content='Loading app'/>;
+
+  return (
+      <Card fluid>
+      <Image src={`/assets/categoryImages/${activity.category}.jpg`}/>
+      <Card.Content>
+        <Card.Header>{activity.title}</Card.Header>
+        <Card.Meta>
+          <span>{activity.date}</span>
+        </Card.Meta>
+        <Card.Description>
+          {activity.description}
+        </Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <Button.Group widths='2'>
+          <Button onClick = {() => openForm(activity.id)} basic color='blue' content='Edit'/>
+          <Button onClick={cancelSelectedActivity} basic color='grey' content='Cancel'/>
+        </Button.Group>
+      </Card.Content>
+    </Card>
+  )
 }
